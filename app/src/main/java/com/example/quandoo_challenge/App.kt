dc327115229fe9069fa253cc49d4376123840942
VictorGTdaCruz <victorgtdacruz@outlook.com>
2019-12-22 15:21:58 -0300

    Add App class with kodein imports

---
+++ app/src/main/java/com/example/quandoo_challenge/App.kt
@@ -0,0 +1,20 @@
+package com.example.quandoo_challenge
+
+import android.app.Application
+import com.example.feature_merchants.di.merchantsServiceModule
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.provider
+
+class App : Application(), KodeinAware {
+
+    private val appModule = Kodein.Module(name = "application") {
+        bind() from provider { this }
+    }
+
+    override val kodein = Kodein {
+        import(appModule)
+        import(merchantsServiceModule)
+    }
+}
